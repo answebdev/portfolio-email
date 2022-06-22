4d1d8c99c4ca0c86e64142c49f149e38c4f2a05b
ANS <36783010+answebdev@users.noreply.github.com>
2022-06-22 08:28:57 -0700

    clean up files

--- src/App.js
+++ src/App.js
@@ -6,13 +6,7 @@ import Footer from './components/layout/Footer';
 // import FooterAlt from './components/layout/FooterAlt';
 import LandingPage from './components/pages/LandingPage';
 import About from './components/pages/About';
-import ProjectsOriginal from './components/pages/ProjectsOriginal';
 import Portfolio from './components/pages/Portfolio';
-import ParticipialAdjectives from './components/pages/ParticipialAdjectives';
-import Customer from './components/pages/Customer';
-import Orientation from './components/pages/Orientation';
-import Netlify from './components/pages/Netlify';
-import CanvasModules from './components/pages/CanvasModules';
 import Contact from './components/pages/Contact';
 import Success from './components/pages/Success';
 import Error from './components/pages/Error';
@@ -30,37 +24,7 @@ const App = () => {
             <Switch>
               <Route exact path='/' component={LandingPage} />
               <Route exact path='/about' component={About} />
-              <Route
-                exact
-                path='/projects-original'
-                component={ProjectsOriginal}
-              />
               <Route exact path='/portfolio' component={Portfolio} />
-              <Route
-                exact
-                path='/portfolio/participial-adjectives'
-                component={ParticipialAdjectives}
-              />
-              <Route
-                exact
-                path='/portfolio/improving-customer-satisfaction'
-                component={Customer}
-              />
-              <Route
-                exact
-                path='/portfolio/new-employee-orientation'
-                component={Orientation}
-              />
-              <Route
-                exact
-                path='/portfolio/using-netlify-to-host-your-web-application'
-                component={Netlify}
-              />
-              <Route
-                exact
-                path='/portfolio/canvas-modules'
-                component={CanvasModules}
-              />
               <Route exact path='/contact' component={Contact} />
               <Route exact path='/success' component={Success} />
               <Route component={Error} />
