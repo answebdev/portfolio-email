c9b5aa931a77a84ca4ebbf02c38ee1d6ec142b4f
ANS <36783010+answebdev@users.noreply.github.com>
2022-06-22 18:53:04 -0700

    add placeholder project cards

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Card, Button } from 'react-bootstrap';
 import { Helmet } from 'react-helmet';
 import classes from '../../styles/Portfolio.module.css';
 
@@ -18,9 +18,125 @@ const Portfolio = () => {
             <Col md={12}>
               <div>
                 <h1 className={classes.PageTitle}>Portfolio</h1>
-                {/* <h5 style={{ fontWeight: '400' }} class='animated zoomIn'>
-                  eLearning &bull; Instructor-Led Training
-                </h5> */}
+                <h5 style={{ fontWeight: '400' }} class='animated zoomIn'>
+                  SAMPLES OF MY WORK
+                </h5>
+              </div>
+            </Col>
+          </Row>
+
+          <br />
+
+          <Row>
+            <Col md={12}>
+              <div className={classes.CardContainer}>
+                <div className={classes.CardDiv}>
+                  <Card className={classes.CardWidth}>
+                    <Card.Img
+                      variant='top'
+                      src='https://images.pexels.com/photos/1114376/pexels-photo-1114376.jpeg?auto=compress&cs=tinysrgb&w=600'
+                    />
+                    <Card.Body>
+                      <Card.Title>Card Title</Card.Title>
+                      <Card.Text>
+                        Some quick example text to build on the card title and
+                        make up the bulk of the card's content.
+                      </Card.Text>
+                      <Button variant='primary'>Go somewhere</Button>
+                    </Card.Body>
+                  </Card>
+                </div>
+
+                <div className={classes.CardDiv}>
+                  <Card className={classes.CardWidth}>
+                    <Card.Img
+                      variant='top'
+                      src='https://images.pexels.com/photos/1086711/pexels-photo-1086711.jpeg?auto=compress&cs=tinysrgb&w=600'
+                    />
+                    <Card.Body>
+                      <Card.Title>Card Title</Card.Title>
+                      <Card.Text>
+                        Some quick example text to build on the card title and
+                        make up the bulk of the card's content.
+                      </Card.Text>
+                      <Button variant='primary'>Go somewhere</Button>
+                    </Card.Body>
+                  </Card>
+                </div>
+
+                <div className={classes.CardDiv}>
+                  <Card className={classes.CardWidth}>
+                    <Card.Img
+                      variant='top'
+                      src='https://images.pexels.com/photos/1031700/pexels-photo-1031700.jpeg?auto=compress&cs=tinysrgb&w=600'
+                    />
+                    <Card.Body>
+                      <Card.Title>Card Title</Card.Title>
+                      <Card.Text>
+                        Some quick example text to build on the card title and
+                        make up the bulk of the card's content.
+                      </Card.Text>
+                      <Button variant='primary'>Go somewhere</Button>
+                    </Card.Body>
+                  </Card>
+                </div>
+              </div>
+            </Col>
+          </Row>
+
+          <Row>
+            <Col md={12}>
+              <div className={classes.CardContainer}>
+                <div className={classes.CardDiv}>
+                  <Card className={classes.CardWidth}>
+                    <Card.Img
+                      variant='top'
+                      src='https://images.pexels.com/photos/1051747/pexels-photo-1051747.jpeg?auto=compress&cs=tinysrgb&w=600'
+                    />
+                    <Card.Body>
+                      <Card.Title>Card Title</Card.Title>
+                      <Card.Text>
+                        Some quick example text to build on the card title and
+                        make up the bulk of the card's content.
+                      </Card.Text>
+                      <Button variant='primary'>Go somewhere</Button>
+                    </Card.Body>
+                  </Card>
+                </div>
+
+                <div className={classes.CardDiv}>
+                  <Card className={classes.CardWidth}>
+                    <Card.Img
+                      variant='top'
+                      src='https://images.pexels.com/photos/2529787/pexels-photo-2529787.jpeg?auto=compress&cs=tinysrgb&w=600'
+                    />
+                    <Card.Body>
+                      <Card.Title>Card Title</Card.Title>
+                      <Card.Text>
+                        Some quick example text to build on the card title and
+                        make up the bulk of the card's content.
+                      </Card.Text>
+                      <Button variant='primary'>Go somewhere</Button>
+                    </Card.Body>
+                  </Card>
+                </div>
+
+                <div className={classes.CardDiv}>
+                  <Card className={classes.CardWidth}>
+                    <Card.Img
+                      variant='top'
+                      src='https://images.pexels.com/photos/210126/pexels-photo-210126.jpeg?auto=compress&cs=tinysrgb&w=600'
+                    />
+                    <Card.Body>
+                      <Card.Title>Card Title</Card.Title>
+                      <Card.Text>
+                        Some quick example text to build on the card title and
+                        make up the bulk of the card's content.
+                      </Card.Text>
+                      <Button variant='primary'>Go somewhere</Button>
+                    </Card.Body>
+                  </Card>
+                </div>
               </div>
             </Col>
           </Row>
